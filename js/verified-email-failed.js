abffc5c8b9c7934e77b4a50e4f8b1f0a85f3c09b
lucky <salawulucky08071@gmail.com>
2025-08-17 21:57:08 +0100

    fix url

--- js/verified-email-failed.js
+++ js/verified-email-failed.js
@@ -1,4 +1,4 @@
-ADMIN_URL = "http://127.0.0.1:8000/admins/api/user"
+ADMIN_URL = "https://luck1999.pythonanywhere.com/admins/api/user"
 document.addEventListener('DOMContentLoaded', function() {
     // Simulate a slight delay for verification
     setTimeout(() => {
