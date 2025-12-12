41adbd4b9410c8b84b1765390f06ff053b72644a
lucky <salawulucky08071@gmail.com>
2025-12-12 06:39:09 +0100

    43

--- js/admin/main.js
+++ js/admin/main.js
@@ -179,6 +179,6 @@ toggle.addEventListener('click', () => {
 }
 
 
-ANALYTICS_URL = "http://127.0.0.1:8000/admins/api/analytics"
-ADMIN_URL = "http://127.0.0.1:8000/admins/api/admin"
-ASO_URL = "http://127.0.0.1:8000/aso/api/product"
+ANALYTICS_URL = "http://192.168.0.200:8000/admins/api/analytics"
+ADMIN_URL = "http://192.168.0.200:8000/admins/api/admin"
+ASO_URL = "http://192.168.0.200:8000/aso/api/product"
